5d715711c3250253323818efdd7860eeab18093f
claudiahW <murungiclaud@gmail.com>
2024-01-07 19:15:48 +0300

    run join method on existing array

--- scripts.js
+++ scripts.js
@@ -79,6 +79,20 @@ const menu = [
 
  window.addEventListener("DOMContentLoaded", function() {
     let displayMenu = menu.map(function(item){
-        return item;
+        return `<article class="menu-item">
+        <img src=${item.img} class="photo"  alt=${item.title} /> 
+        <div class="item-info">
+           <header>
+           <h4>${item.title}</h4>
+           <h4 class="price">$${item.price}</h4>
+       </header>
+       <p class="item-text">
+        ${item.desc}
+       </p>
+        </div>  
+       </article>`;
     });
+    displayMenu = displayMenu.join("");
+    sectionCenter.innerHTML = displayMenu;
+    
  });
